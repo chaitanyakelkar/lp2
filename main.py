5e44004d127d0afb822ba593cf3bb6a45ab3c0fd
Chaitanya Kelkar <chaitanyaakelkar57@gmail.com>
2025-04-21 18:40:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,25 +4,26 @@ while True:
   print("2.Subtract")
   print("3.Multiply")
   print("4.Divide")
+  print("5.Exit")
   inp = int(input("Enter your input: "))
 
   if (inp == 1):
-  elif (inp == 2):
     a = int(input("Enter No 1: "))
     b = int(input("Enter No 2: "))
     print(f"{a} + {b} = {a+b}")
-  elif (inp == 3):
+  elif (inp == 2):
     a = int(input("Enter No 1: "))
     b = int(input("Enter No 2: "))
     print(f"{a} - {b} = {a-b}")
-  elif (inp == 4):
+  elif (inp == 3):
     a = int(input("Enter No 1: "))
     b = int(input("Enter No 2: "))
     print(f"{a} x {b} = {a*b}")
-  elif (inp == 5):
+  elif (inp == 4):
     a = int(input("Enter No 1: "))
     b = int(input("Enter No 2: "))
     print(f"{a} / {b} = {a/b}")
+  elif (inp == 5):
     print("Bye!")
     break
   else:
